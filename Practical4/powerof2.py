63d878da6640d7edef04083f8f64898f639951eb
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-03-13 19:52:25 +0800

    complete powerof2.py
    outcome is one one line
    no error

--- Practical4/powerof2.py
+++ Practical4/powerof2.py
@@ -5,20 +5,30 @@ Created on Wed Mar 13 10:27:05 2019
 @author: sissy
 """
 
+#assign x as a positive integer
 x=2019
+#a=x
 a=2019
+#write the template of the outcome 
+b=str(a)+" is "
+c="2**"
 #find the biggest 2**n that is smaller than x
+#power no bigger than 13
 n=13   
+#x is possitive
 while x!=0:
+#make sure n won't be a negative number
     if n>=0:
         if x == 2**n:
-            print("+2**",n)
+            b=b+c+str(n)
             break
         elif x > 2**n:
             x=x-2**n
-            print("+2**",n)
+            b=b+c+str(n)+"+"
             n=n-1
         elif x < 2**n:
             n=n-1
     else:
         break
+#outcome
+print(b)
