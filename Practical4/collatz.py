4697dfe1da562bd4b695cac61aaa28151d35cd0b
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-03-13 10:18:14 +0800

    Add collatz.py file

---
+++ Practical4/collatz.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:51:54 2019
+
+@author: sissy
+"""
+
+#start from a positive integer n
+n=27
+#if n is even
+#n=n/2
+while 1==1:
+    if n%2==0:
+        n=n/2
+        print(n)
+#if n is odd
+#n=n*3+1
+    elif n==1:
+        break
+    elif n%2==1:
+        n=n*3+1
+        print(n)
+    #print n
+    
