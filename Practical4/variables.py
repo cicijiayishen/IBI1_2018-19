bc33c2803b56b6dfb28ba5e686710e6cd51694bb
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-03-13 09:51:22 +0800

    Add variables.py file

---
+++ Practical4/variables.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:27:22 2019
+
+@author: sissy
+"""
+
+#Variables
+a = 412
+b = 412412
+print(b % 7)
+c = b/7
+print(c)
+d = c/11
+print(d)
+e = d/13
+print(e)
+print(a == e)
+
+#Booleans
+X=False
+Y=True
+Z = (X and not Y) or (Y and not X)
+Z
+W=X!=Y
+W==Z
