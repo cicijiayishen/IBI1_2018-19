b8a5f9e86af8775abba5d846cc0651b09ab90c58
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-04-11 10:18:14 +0800

    Change the root of the file

--- Practical8/GO.py
+++ Practical8/GO.py
@@ -7,7 +7,7 @@ Created on Wed Apr 10 09:05:01 2019
 
 from xml.dom.minidom import parse
 import  xml.dom.minidom
-DOMTree = xml.dom.minidom.parse(r'C:\Users\sissy\Desktop\IBI P8\go_obo.xml')
+DOMTree = xml.dom.minidom.parse(r'C:\Users\sissy\Desktop\test Git\IBI1_2018-19\Practical8\go_obo.xml')
 #dom = xml.dom.minidom.parse(r'C:\Users\sissy\Desktop\IBI P8\go_obo.xml')
 collection = DOMTree.documentElement
 #<?xml version="1.0" encoding="UTF-8"?>
