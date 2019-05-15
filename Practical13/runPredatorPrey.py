4086f7c6ef9dcd7445fa1ca5d3652b460da5513a
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-05-15 17:10:17 +0800

    Practical 13 updated

--- Practical13/runPredatorPrey.py
+++ Practical13/runPredatorPrey.py
@@ -4,11 +4,62 @@ Created on Wed May 15 09:12:42 2019
 
 @author: sissy
 """
-
 import os
 import numpy
 import matplotlib.pyplot as plt
 import re
+
+def xml_to_cps():
+    import os
+    import xml.dom.minidom
+    
+    # first, convert xml to cps 
+    os.system("CopasiSE.exe -i predator-prey.xml -s predator-prey.cps")
+    
+    # now comes the painful part. Just copy and paste this ok
+    
+    cpsTree = xml.dom.minidom.parse("predator-prey.cps")
+    cpsCollection = cpsTree.documentElement
+    
+    reportFile = xml.dom.minidom.parse("report_ref.xml")
+    reportLine = reportFile.documentElement
+    
+    tasks = cpsCollection.getElementsByTagName("Task")
+    for task in tasks:
+        if task.getAttribute("name")=="Time-Course":
+            task.setAttribute("scheduled","true")
+            task.insertBefore(reportLine,task.childNodes[0])
+            break
+        
+    
+    for taskDetails in task.childNodes:
+        if taskDetails.nodeType ==1:
+            if taskDetails.nodeName == "Problem":
+                problem = taskDetails
+                
+    for param in problem.childNodes:
+        if param.nodeType ==1:
+            if param.getAttribute("name")=="StepNumber":
+                param.setAttribute("value","200")
+            if param.getAttribute("name")=="StepSize":
+                param.setAttribute("value","1")
+            if param.getAttribute("name")=="Duration":
+                param.setAttribute("value","200")
+           
+            
+    report18 = xml.dom.minidom.parse("report18.xml")
+    report = report18.documentElement
+    
+    listOfReports  =  cpsCollection.getElementsByTagName("ListOfReports")[0]
+    listOfReports.appendChild(report)
+    
+    cpsFile = open("predator-prey.cps","w",encoding='utf-8')
+    cpsTree.writexml(cpsFile)
+    cpsFile.close()        
+
+
+os.system('CopasiSE.exe predator-prey.cps')
+
 os.chdir("C:/Users/sissy/Desktop/test Git/IBI1_2018-19/Practical13")
 
 csvfile=open('modelResults.csv','r')
@@ -29,11 +80,35 @@ results=numpy.array(list)
 
 
 results=results.astype(numpy.float)
-
+#Plot time-population curve
 plt.plot(results[:,0],label='Predator (b=0.02, d=0.4)')
 plt.plot(results[:,1],label='Prey (b=0.1, d=0.02)')
 plt.xlabel('time')
 plt.ylabel('population size')
 plt.legend()
 plt.show()
+#Plot Limit cycle curve
+plt.plot(results[:,0],results[:,1])
+plt.xlabel('predator population')
+plt.ylabel('prey population')
+plt.title('Limit cycle')
+plt.show()
+#Changing parameters
+import xml.dom.minidom
+DOMTree=xml.dom.minidom.parse('C:/Users/sissy/Desktop/test Git/IBI1_2018-19/Practical13/predator-prey.xml')
+collection=DOMTree.documentElement
+parameters=collection.getElementsByTagName('parameter')
+set=[]
+for p in parameters:
+    print(p.getAttribute('id'))
+    v=input('Please input a number from 0 to 1 to change the parameter:')
+    p.setAttribute('value',v)
+    print(p.getAttribute('value'))
+xml_to_cps()
+#Run the cps file in Copasi
+#Get a csv file
+#Use the csv file to plot 2 curves
 
+#Running many simulations
+#for i in range(0,100)
+#do the same thing as 'Changing parameters'
