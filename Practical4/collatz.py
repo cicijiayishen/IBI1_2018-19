40e2bb770735825d93ad152e6f1bc2671ed4ffd9
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-05-15 08:24:33 +0800

    Ask the user to input a number

--- Practical4/collatz.py
+++ Practical4/collatz.py
@@ -6,19 +6,21 @@ Created on Wed Mar 13 09:51:54 2019
 """
 
 #start from a positive integer n
-n=27
-#if n is even
-#n=n/2
-while 1==1:
-    if n%2==0:
-        n=n/2
-        print(n)
-#if n is odd
-#n=n*3+1
-    elif n==1:
-        break
-    elif n%2==1:
-        n=n*3+1
-        print(n)
-    #print n
-    
+n=int(input('Please input a positive integer:'))
+if n<=0:
+    print('Error: The number is not positive.')
+elif n>0:
+    #if n is even
+    #n=n/2
+    while 1==1:
+        if n%2==0:
+            n=n/2
+            print(n)
+    #if n is odd
+    #n=n*3+1
+        elif n==1:
+            break
+        elif n%2==1:
+            n=n*3+1
+            print(n)
+        #print n
