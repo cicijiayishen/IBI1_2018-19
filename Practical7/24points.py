09da1aa5ec13cbc6a6d523a1c945d143973486c6
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-05-16 22:11:21 +0800

    Add comments to Practical 7

--- Practical7/24points.py
+++ Practical7/24points.py
@@ -4,17 +4,15 @@ Created on Wed Apr  3 09:01:11 2019
 
 @author: sissy
 """
-
+#import necessary libraries
 import re
-from fractions import Fraction
-re_numtest=re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)')
-#input the numbers
+from fractions import Fraction#numbers may be fractions
+re_numtest=re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)')#check if numbers are within 1-23
 i=1
 while i:
     i=0
-    L=input("Please input numbers to compute 24:(use ',' to divide them)")
-    L_list=L.split(",")
-#check whether the numbers are in range 1 to 23
+    L=input("Please input numbers to compute 24:(use ',' to divide them)")#input numbers
+    L_list=L.split(",")#split
     for char in L_list:
         if re_numtest.match(char):
             continue
@@ -22,15 +20,14 @@ while i:
             print('The input number must be intergers from 1 to 23')
             i=1
             break
-num = list(map(int,L_list))
+num = list(map(int,L_list))#use int() function to all elements in L_list
 count=0
-
 def points(n):
     global count
-    count = count +1
+    count = count +1#count recursion times
     
     if n == 1:
-        if(float(num[0])==24):
+        if(float(num[0])==24):#if the number is 24, exit the loop
             return 1
         else:
             return 0
