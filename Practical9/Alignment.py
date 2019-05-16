1ceea62ce4c1e2ff84f2344e8a9830a5a6e2599c
Cici Jiayi Shen <jiayis.18@intl.zju.edu.cn>
2019-05-16 22:18:54 +0800

    Add comments to Practical 9

--- Practical9/Alignment.py
+++ Practical9/Alignment.py
@@ -5,20 +5,19 @@ Created on Wed Apr 17 09:10:59 2019
 @author: cici
 Practical 9: Implementation of a sequence comparison program in Python
 """
-#create a list of lists containing BLOSUM62
+#import necessary libraries
 import pandas as pd
+#open csv file
 data=pd.read_csv('C:/Users/sissy/Desktop/test Git/IBI1_2018-19/Practical9/BLOSUM62.txt',sep=' +',engine='python')
-m=data.to_dict()
-myDict={'A':1,'R':2,'N':3,'D':4,'C':5,'Q':6,'E':7,'G':8,'H':9,'I':10,'L':11,'K':12,'M':13,'F':14,'P':15,'S':16,'T':17,'W':18,'Y':19,'V':20,'B':21,'Z':22,'X':23}
+m=data.to_dict()#change to a dictionary
 #input sequences
 seq01=input("Give me a protein sequence:")
 seq02=input("GIve me a protein sequence:")
 #split the sequences
 seq1=seq01.replace('\n','')
 seq2=seq02.replace('\n','')
-#set initial distance as zero
 score=0 #set initial score as zero
-count=0
+count=0 #set initial count as zero
 ali=[]#alignment
 for i in range(len(seq1)): #compare each amino acid
     if seq1[i]!=seq2[i]:
@@ -36,7 +35,7 @@ percentage=count/len(seq1)
 #print out the results
 print("Sequence 1:",seq01)
 print("Alignment:",ali)
-print("Sequence:",seq02)
+print("Sequence 2:",seq02)
 print("The BLOSUM62 score is:",score)
 print("The percentage identity is:",percentage)
 print("The normalised BLOSUM62 score is:",score/len(seq1))
